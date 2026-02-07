f29ed3aff47255b6d34b299b03d553fedcf13fbd
dawson <dawsoncarsoulle@gmail.com>
2026-02-07 15:06:18 +0100

    feat: Add generic shape method and helpers(closes #19)

--- src/turtle.rs
+++ src/turtle.rs
@@ -50,7 +50,7 @@ impl Turtle {
         Turtle {
             point: Point::new(),
             angle: 0.0,
-            writing: true,
+            writing: false,
             lines: Vec::new(),
             color: Color::Black,
             width: 2.0,
@@ -73,6 +73,14 @@ impl Turtle {
     }
 
     pub fn goto(&mut self, x: f64, y: f64) -> &mut Self {
+        if self.writing {
+            self.lines.push(Line {
+                start: self.point,
+                end: Point::new_point(x, y),
+                color: self.color,
+                width: self.width,
+            });
+        }
         self.point.point.0 = x;
         self.point.point.1 = y;
         self
@@ -105,6 +113,62 @@ impl Turtle {
 
     pub fn set_pen_width(&mut self, width: f64) -> &mut Self {
         self.width = width;
+        self
+    }
+
+    pub fn shape<F>(&mut self, radius: f64, segments: usize, deformation: F) -> &mut Self
+    where
+        F: Fn(usize) -> f64,
+    {
+        let (cx, cy) = self.point.point;
+        let angle_0: f64 = self.angle;
+        let radius_0 = radius * deformation(0);
+        let x_0 = cx + radius_0 * angle_0.to_radians().cos();
+        let y_0 = cy + radius_0 * angle_0.to_radians().sin();
+
+        self.pen_up();
+        self.goto(x_0, y_0);
+        self.pen_down();
+        for i in 1..=segments {
+            let angle = i as f64 * (360.0 / segments as f64) + self.angle;
+            let actual_radius = radius * deformation(i % segments);
+            let target_x = cx + actual_radius * angle.to_radians().cos();
+            let target_y = cy + actual_radius * angle.to_radians().sin();
+            self.goto(target_x, target_y);
+        }
+        self.pen_up();
+        self
+    }
+
+    pub fn circle(&mut self, radius: f64) -> &mut Self {
+        self.shape(radius, 360, |_| 1.0);
+        self
+    }
+
+    pub fn square(&mut self, size: f64) -> &mut Self {
+        self.right(45.0);
+        self.shape(1.0, 4, |_| 1.0 * size);
+        self.left(45.0);
+        self
+    }
+
+    pub fn triangle(&mut self, size: f64) -> &mut Self {
+        self.right(90.0);
+        self.shape(1.0, 3, |_| 1.0 * size);
+        self.left(90.0);
+        self
+    }
+
+    pub fn star(&mut self, size: f64, branch: usize) -> &mut Self {
+        if branch * 2 < 6 {
+            println!("error : the number of branch must be superior or equal to 3");
+            return self;
+        }
+        self.right(90.0);
+        self.shape(1.0, branch * 2, |i| {
+            if i % 2 == 0 { 1.0 * size } else { 0.4 * size }
+        });
+        self.left(90.0);
         self
     }
 
