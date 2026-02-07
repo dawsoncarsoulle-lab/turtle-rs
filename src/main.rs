990d60ed48a12dd685e201a2b7c4701b2ae6feb3
dawson <dawsoncarsoulle@gmail.com>
2026-02-07 11:05:59 +0100

    refactor: Extract Line struct and enable method chaining (closes #17)

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ fn main() {
     tortue.goto(500.0, 500.0);
     let mut width: f64 = 2.0;
     for line in 0..=3 {
-        tortue.set_pen_width(width);
-        tortue.set_color(Color::Green);
-        tortue.forward(50.0);
-        tortue.set_color(Color::Blue);
-        tortue.forward(50.0);
+        tortue
+            .set_pen_width(width)
+            .forward(50.0)
+            .left(90.0)
+            .set_color(Color::Purple)
+            .right(90.0);
+
         width += 4.0;
     }
     tortue.save_svg("dessin.svg");
